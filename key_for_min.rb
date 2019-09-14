41d0e2584366bf3cca614f3927ab8ce16ea48be6
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 13:08:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key, value|
     
-    if value < min
+    if value < min || value == nil
       min = value
       min_key = key
     end
